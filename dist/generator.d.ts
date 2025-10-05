04fd6edf1fa909b799616eb94e906fa8bc3e15c2
r-werner <rwerner@gmx.de>
2025-10-05 19:17:44 +0200

    Add .gitignore and remove dist files

--- dist/generator.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Generate Python source code from a Python AST.
- *
- * @param pythonAst - Python AST node (typically Module from py-ast)
- * @returns Python source code as a string
- */
-export declare function generatePython(pythonAst: any): string;
-//# sourceMappingURL=generator.d.ts.map
